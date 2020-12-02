2776cffbfa4e5adc6288d63b41bf155038635801
Gus McCoy <51655110+gusmccoy@users.noreply.github.com>
2020-12-02 14:46:46 -0600

    Working on a save state for in between scenes data

---
+++ Assets/Scripts/SaveState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveState
+{
+    // WILL BE USED TO RETURN PLAYER TO CORRECT COORDIATES AFTER MAIN SCENE IS RELOADED
+    public static float playerCoordinateX { get; set; }
+    public static float playerCoordinateY { get; set; }
+
+    // WILL BE USED TO GET THE CORRECT SPRITES
+    public static int allyID { get; set; }
+    public static int enemyID { get; set; }
+
+    // WILL BE USED TO DETERMINE IF GAME IS OVER/INFORM USER WHAT CREATURES THEY'VE CAPTURED
+    public static bool[] capturedCreatures { get; set; }
+
+}
